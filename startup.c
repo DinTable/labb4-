5659b9981bc221d0744297c9cc5e5eb1088e84fb
AttilaLundin <99827331+AttilaLundin@users.noreply.github.com>
2022-02-17 00:52:21 +0100

    Update startup.c

--- startup.c
+++ startup.c
@@ -3,14 +3,16 @@
  *
  */
 __attribute__((naked)) __attribute__((section (".start_section")) )
-void startup ( void )
-{
+void startup ( void ){
 __asm__ volatile(" LDR R0,=0x2001C000\n");		/* set stack */
 __asm__ volatile(" MOV SP,R0\n");
 __asm__ volatile(" BL main\n");					/* call main */
 __asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
+/*
+ * Systick  
+ */
 #define STK_CTRL ((volatile unsigned int *)(0xE000E010))  
 #define STK_LOAD ((volatile unsigned int *)(0xE000E014))  
 #define STK_VAL ((volatile unsigned int *)(0xE000E018)) 
@@ -27,20 +29,38 @@ __asm__ volatile(".L1: B .L1\n");				/* never return */
 
 #define SIMULATOR 1
 
-#define SYSCFG_EXTICR1 ((short *) (0x40013880))
+#define SYSCFG_EXTICR1 ((short *) (0x40013808))
 #define EXTI_IRM ((int *)(0x40013C00))
 #define EXTI_RTSR ((int *)(0x40013C08))
+#define EXTI_PR ((int *)(0x40013C14))
+#define NVIC_ISER0 ((int *)(0xE000E100))
+#define EXTI3_IRQ (0x2001C000 + 0x00000064)
+
+static int count = 0; 
+
+void irq_handler(void){
+	++count;
+	*EXTI_PR |= (1<<3); //kvitterar avbrott
+}
 
 void app_init(){
-	*GPIO_MODER = 0x00005555; 
+	*GPIO_MODER = 0x00005555;	//initierar PD0-7
+	
+	*SYSCFG_EXTICR1 &= ~0xF000;  //nollställer bitfält för EXTI3 
+	*SYSCFG_EXTICR1 |= 0x4000;	//port E till  EXTI3
+	
+	*EXTI_IRM |= (1<<3);		//aktiverar avbrottslina 3
+	*EXTI_RTSR |= (1<<3);		//aktivera avbrott på positiv flank på avbrottslina 3
+	
+	*NVIC_ISER0 |= (1<<9);		//aktiverar avbrott från 
+	*((void (**)(void)) EXTI3_IRQ) = irq_handler; //kopplar avbrottsrutin till EXTI3
+	
+	
 }
 
 void main(void){
-	*SYSCFG_EXTICR1 &= 0x0FFF;
-	*SYSCFG_EXTICR1 |= 0x4000;
-	*EXTI_IRM |= (1<<3);
-	*EXTI_RTSR |= (1<<3);
-	
-	
+	app_init();
+	while(1){
+		*GPIO_ODR_LOW = count; 
+	}
 }
-
